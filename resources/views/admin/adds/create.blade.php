f0c349b1d4ff6c6407450145bb54d2937972248d
Your Name <you@example.com>
2022-06-03 17:57:51 +0300

    fix

--- resources/views/admin/adds/create.blade.php
+++ resources/views/admin/adds/create.blade.php
@@ -1,11 +1,6 @@
 @extends('admin.layout.app')
 @push('css')
-    <style>
-        #map {
-            height: 300px;
-            border: 1px solid #000;
-        }
-    </style>
+
 
 @endpush
 @section('content')
@@ -65,7 +60,19 @@
                                                     <label class="col-xl-3 col-lg-3 col-form-label">{{__('views.image')}} </label>
                                                     <div class="col-lg-9 col-xl-6">
                                                         <div class="input-group validated">
-                                                            <div id="map"></div>
+                                                            <fieldset>
+                                                                <legend>{{""}}</legend>
+                                                                <input id="searchInput" class="input-controls" type="text"
+                                                                       placeholder="Enter a location">
+                                                                <div class="map" id="map" style="width: 100%; height: 300px;"></div>
+                                                                <div class="form_area">
+                                                                    <input type="text" disabled value="{{$setting->address}}" name="address" id="location">
+                                                                    <input type="text" value="{{$setting->latitude}}" name="latitude" id="lat">
+                                                                    <input type="text" value="{{$setting->longitude}}" name="longitude" id="lng">
+                                                                </div>
+
+                                                            </fieldset>
+
                                                         </div>
                                                     </div>
                                                 </div>
@@ -123,5 +130,75 @@
 
 @push('scripts')
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtdvEJAz8r2k5V6Q618BosZla0iGrdMqU&sensor=false"></script>
+<script>
+    function initialize() {
+        var latlng = new google.maps.LatLng('{{$setting->latitude}}', '{{$setting->longitude}}');
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: latlng,
+            zoom: 10,
+            gestureHandling: 'greedy'
+        });
+        var marker = new google.maps.Marker({
+            map: map,
+            position: latlng,
+            draggable: true,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
+        var input = document.getElementById('searchInput');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var geocoder = new google.maps.Geocoder();
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+        var infowindow = new google.maps.InfoWindow();
+        autocomplete.addListener('place_changed', function () {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                window.alert("Autocomplete's returned place contains no geometry");
+                return;
+            }
 
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);
+            }
+
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+
+            bindDataToForm(place.formatted_address, place.geometry.location.lat(), place.geometry.location.lng());
+            infowindow.setContent(place.formatted_address);
+            infowindow.open(map, marker);
+
+        });
+        // this function will work on marker move event into map
+        google.maps.event.addListener(marker, 'dragend', function () {
+            geocoder.geocode({'latLng': marker.getPosition()}, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    if (results[0]) {
+                        bindDataToForm(results[0].formatted_address, marker.getPosition().lat(), marker.getPosition().lng());
+                        infowindow.setContent(results[0].formatted_address);
+                        infowindow.open(map, marker);
+                    }
+                }
+            });
+        });
+    }
+
+    function bindDataToForm(address, lat, lng) {
+        document.getElementById('location').value = address;
+        document.getElementById('lat').value = lat;
+        document.getElementById('lng').value = lng;
+//                                                console.log('location = ' + address);
+//                                                console.log('lat = ' + lat);
+//                                                console.log('lng = ' + lng);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
 @endpush
